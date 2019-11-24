d4c9b3fef385110b5787792eb78323f376a39e52
Ricardo Boss <contact@ricardoboss.de>
2019-11-24 16:00:42 +0100

    Installed Laravel Dusk and removed frontend scaffolding
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public final function testLogin(): void
+    {
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/')
+                ->assertPathIs('/login')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+    }
+}
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,17 +3,20 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
     /**
      * Creates the application.
      *
-     * @return \Illuminate\Foundation\Application
+     * @return Application
      */
-    public function createApplication()
+    public final function createApplication(): Application
     {
         $app = require __DIR__ . '/../bootstrap/app.php';
+
+        assert($app instanceof Application);
 
         $app->make(Kernel::class)->bootstrap();
 
