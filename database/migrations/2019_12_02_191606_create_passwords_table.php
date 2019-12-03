36e4ccf2c9762be94a2f27c962575e0d5083c0f5
Ricardo Boss <contact@ricardoboss.de>
2019-12-03 01:33:40 +0100

    Added migration for passwords
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

---
+++ database/migrations/2019_12_02_191606_create_passwords_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasswordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->bigInteger('version')->default(0);
+            $table->string('name');
+            $table->mediumText('notes');
+            $table->mediumText('value');
+
+            $table->timestamp('created_at')->nullable();
+            $table->bigInteger('created_by')->unsigned()->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->bigInteger('updated_by')->unsigned()->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->bigInteger('deleted_by')->unsigned()->nullable();
+
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+}
