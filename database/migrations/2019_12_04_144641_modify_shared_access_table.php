647676d1c59882ca3ea45750da345db7c132a832
Ricardo Boss <ricardo.boss@web.de>
2019-12-04 16:52:59 +0100

    Modify shared_access table

---
+++ database/migrations/2019_12_04_144641_modify_shared_access_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifySharedAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shared_access', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+
+        Schema::table('shared_access', function (Blueprint $table) {
+            $table->string('password_id', 36)->change();
+            $table->morphs('model');
+
+            $table->string('key')->unique()->change();
+            $table->boolean('can_edit')->default(false)->change();
+
+            $table->primary(['password_id', 'model_id', 'model_type']);
+
+            $table->foreign('password_id')->references('id')->on('passwords')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shared_access', function (Blueprint $table) {
+            $table->dropForeign(['password_id']);
+
+            $table->dropPrimary(['password_id', 'model_id', 'model_type']);
+
+            $table->dropColumn('model_id');
+            $table->dropColumn('model_type');
+            $table->dropColumn('password_id');
+        });
+
+        Schema::table('shared_access', function (Blueprint $table) {
+            $table->unsignedBigInteger('password_id');
+            $table->unsignedBigInteger('user_id');
+        });
+    }
+}
