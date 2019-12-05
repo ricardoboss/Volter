cb83a01e8cfc1dbd51f61f36346833185414e2da
Ricardo Boss <ricardo.boss@web.de>
2019-12-05 14:59:59 +0100

    Move model docblocks to other file

--- app/Models/Password.php
+++ app/Models/Password.php
@@ -7,39 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
- * App\Models\Password
- *
- * @property string $id
- * @property int $version
- * @property string $name
- * @property string $notes
- * @property string $value
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property int|null $created_by
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property int|null $updated_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property int|null $deleted_by
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password newQuery()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Password onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password query()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereNotes($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereValue($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Password whereVersion($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Password withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Password withoutTrashed()
- * @mixin \Eloquent
+ * Class Password
+ * @package App\Models
  */
 class Password extends Model
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,34 +10,8 @@ use jeremykenedy\LaravelRoles\Traits\HasRoleAndPermission as HasRoleAndPermissio
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
- * App\Models\User
- *
- * @property int $id
- * @property string $name
- * @property string $email
- * @property \Illuminate\Support\Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\jeremykenedy\LaravelRoles\Models\Role[] $roles
- * @property-read int|null $roles_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\jeremykenedy\LaravelRoles\Models\Permission[] $userPermissions
- * @property-read int|null $user_permissions_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
- * @mixin \Eloquent
+ * Class User
+ * @package App\Models
  */
 class User extends Authenticatable implements JWTSubject, HasRoleAndPermissionInterface
 {
