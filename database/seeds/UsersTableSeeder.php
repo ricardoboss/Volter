b4a3d02d199d3bb0d335a2411c37ae41f710ed75
Ricardo Boss <contact@ricardoboss.de>
2019-12-09 00:24:38 +0100

    Added ability to seed user from .env file
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (
+            env('SEED_NAME') != null &&
+            env('SEED_PASS') != null &&
+            env('SEED_MAIL') != null &&
+            !DB::table('users')->where(['email' => env('SEED_MAIL')])->exists()
+        )
+            factory(User::class)->create([
+                'name' => env('SEED_NAME'),
+                'email' => env('SEED_MAIL'),
+                'password' => bcrypt(env('SEED_PASS')),
+                'email_verified_at' => Carbon::now(),
+            ]);
+
+        factory(User::class, 20)->create();
+    }
+}
