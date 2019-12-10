25bff6c257a61697f4ae9bd87db5a9d51a010cc0
Ricardo Boss <contact@ricardoboss.de>
2019-12-10 23:58:03 +0100

    Updated design of login form
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,19 +1,46 @@
 <template>
-    <div>
-        <login-form v-on:login-success="loggedIn"/>
+    <div id="wrapper">
+        <div id="inner-wrapper">
+            <h1 class="display-1 text-center mb-4">Volter</h1>
+
+            <div class="card shadow-lg mx-auto" id="login-form-container">
+                <div class="card-body">
+                    <login-form v-on:login-success="onLogin"/>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import LoginForm from "../components/LoginForm";
+    import {homeRoute} from "../router";
 
     export default {
         name: "Login",
         components: {LoginForm},
         methods: {
-            async loggedIn() {
-                await this.$router.push({name: 'home'});
+            async onLogin() {
+                await this.$router.push(homeRoute);
             }
         }
     }
 </script>
+
+<style>
+    #wrapper {
+        position: relative;
+        height: 100%;
+    }
+
+    #inner-wrapper {
+        position: relative;
+        top: 50%;
+
+        transform: translateY(-50%);
+    }
+
+    #login-form-container {
+        max-width: 400px;
+    }
+</style>
