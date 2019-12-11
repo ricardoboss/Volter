f11a7642d653617aff2871d01295131165fe7018
ricardoboss <contact@ricardoboss.de>
2019-12-11 17:43:02 +0100

    Remove buttons from home

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Value</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="password in this.all">
-                    <td>
-                        <pre>{{ password.id }}</pre>
-                    </td>
-                    <td>{{ password.name }}</td>
-                    <td>{{ password.value }}</td>
-                    <td>
-                        <div class="btn-group">
-                            <button @click.prevent="edit(password)" class="btn btn-link">edit</button>
-                        </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="btn-group">
-            <button @click.prevent="loadMore" class="btn btn-outline-secondary">Load more</button>
-            <button @click="logout" class="btn btn-outline-danger">Logout</button>
-        </div>
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="password in this.all">
+                <td>
+                    <pre>{{ password.id }}</pre>
+                </td>
+                <td>{{ password.name }}</td>
+                <td>{{ password.value }}</td>
+                <td>
+                    <div class="btn-group">
+                        <button @click.prevent="edit(password)" class="btn btn-link">edit</button>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <b-spinner class="mx-auto" type="grow" v-b-visible="loadMore" variant="primary"/>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -43,19 +41,6 @@
         },
 
         methods: {
-            logout() {
-                this.$store.dispatch('auth/logout');
-
-                this.$swal({
-                    toast: true,
-                    text: "Goodbye!",
-                    timer: 3000,
-                    type: "success",
-                    showConfirmButton: false,
-                    position: "top"
-                });
-            },
-
             async loadMore() {
                 await this.$store.dispatch('passwords/fetchNextPage');
             },
