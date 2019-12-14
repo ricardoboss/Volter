a813f669b904961fc17f19cdd64c6eda1d4a7c3e
Ricardo Boss <contact@ricardoboss.de>
2019-12-14 21:36:16 +0100

    Set less padding on cards on Dashboard
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -5,13 +5,14 @@
         <h2 class="mb-3">Manage</h2>
 
         <b-card-group deck>
-            <b-card align="center" bg-variant="primary">
+            <b-card align="center" bg-variant="primary" body-class="p-1">
                 <b-card-text>
-                    <router-link :to="{ name: 'passwords' }" class="btn btn-link text-light">Manage Passwords
+                    <router-link :to="{ name: 'passwords' }" class="btn btn-link text-light">
+                        Manage Passwords
                     </router-link>
                 </b-card-text>
             </b-card>
-            <b-card align="center" bg-variant="warning">
+            <b-card align="center" bg-variant="warning" body-class="p-1">
                 <a href="#" class="btn btn-link text-dark">Manage Users</a>
             </b-card>
         </b-card-group>
