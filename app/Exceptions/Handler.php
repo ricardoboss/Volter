5bfac568d3739402e261089dcb5f370721377b7b
Ricardo Boss <contact@ricardoboss.de>
2019-12-14 01:32:42 +0100

    Updated exception renderer to match API response schema
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,10 +1,20 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Exceptions;
 
+use App\Enums\ApiErrorCode;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+/**
+ * Class Handler
+ *
+ * @package App\Exceptions
+ */
 class Handler extends ExceptionHandler
 {
     /**
@@ -29,8 +39,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param \Exception $exception
      * @return void
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -40,12 +50,45 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Exception $exception
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Prepare a JSON response for the given exception.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    protected function prepareJsonResponse($request, Exception $e)
+    {
+        $status = $this->isHttpException($e) ? $e->getStatusCode() : 500;
+        $error = ApiErrorCode::exception();
+        if ($status == 401)
+            $error = ApiErrorCode::unauthenticated();
+        else if ($status == 403)
+            $error = ApiErrorCode::unauthorized();
+        else if ($status == 404)
+            $error = ApiErrorCode::not_found();
+        else if ($status == 429)
+            $error = ApiErrorCode::too_many_requests();
+        else if ($status >= 500)
+            $error = ApiErrorCode::server_error();
+
+        return new JsonResponse(
+            [
+                'success' => false,
+                'result' => $this->convertExceptionToArray($e),
+                'error' => $error,
+                'messages' => [$e->getMessage()],
+            ],
+            $status,
+            $this->isHttpException($e) ? $e->getHeaders() : [],
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
     }
 }
