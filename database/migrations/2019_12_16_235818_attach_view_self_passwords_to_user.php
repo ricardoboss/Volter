aa6403c8667965d26cba87554d3aef8018ccc562
Ricardo Boss <contact@ricardoboss.de>
2019-12-17 01:04:08 +0100

    Attach password.view.self permission to user role
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

---
+++ database/migrations/2019_12_16_235818_attach_view_self_passwords_to_user.php
@@ -0,0 +1,10 @@
+<?php
+
+use ricardoboss\Database\Migrations\RolesMigration;
+
+class AttachViewSelfPasswordsToUser extends RolesMigration
+{
+    protected $toAttach = [
+        'user' => ['password.view.self']
+    ];
+}
