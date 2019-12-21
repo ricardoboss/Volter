feb3abd060ab7dd7f450bc432cf1acebd4807609
Ricardo Boss <contact@ricardoboss.de>
2019-12-21 03:21:03 +0100

    Use `allow` and `deny` instead of booleans.
    Signed-off-by: Ricardo Boss <contact@ricardoboss.de>

--- app/Policies/PasswordPolicy.php
+++ app/Policies/PasswordPolicy.php
@@ -7,6 +7,7 @@ namespace App\Policies;
 use App\Models\Password;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\DB;
 
 /**
@@ -19,26 +20,30 @@ class PasswordPolicy
     /**
      * Whether a user can create a new password.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        return $user->hasPermission('password.create');
+        if ($user->hasPermission('password.create')) {
+            return $this->allow("You are able to create passwords.");
+        }
+
+        return $this->deny("You are unauthorized to create passwords.");
     }
 
     /**
      * Whether a user can update a password.
      */
-    public function update(User $user, Password $password): bool
+    public function update(User $user, Password $password): Response
     {
         if ($user->hasPermission('password.edit.any')) {
-            return true;
+            return $this->allow("You can edit any password.");
         }
 
         if ($user->hasPermission('password.edit.self') &&
             $password->created_by === $user->id) {
-            return true;
+            return $this->allow("You can edit this password.");
         }
 
-        return DB::table('shared_access')
+        $shared = DB::table('shared_access')
             ->where([
                 'password_id' => $password->id,
                 'model_type' => User::class,
@@ -46,87 +51,103 @@ class PasswordPolicy
                 'can_edit' => true,
             ])
             ->exists();
+
+        if ($shared) {
+            return $this->allow("This password was shared with you and you can edit it.");
+        }
+
+        return $this->deny("You are unauthorized to update this password.");
     }
 
     /**
      * Whether a user can delete a password.
      */
-    public function delete(User $user, Password $password): bool
+    public function delete(User $user, Password $password): Response
     {
         if ($user->hasPermission('password.delete.any')) {
-            return true;
+            return $this->allow("You can delete any password.");
         }
 
         if ($user->hasPermission('password.delete.self') &&
             $password->created_by === $user->id) {
-            return true;
+            return $this->allow("You can delete this password.");
         }
 
-        return false;
+        return $this->deny("You are unauthorized to delete this password.");
     }
 
     /**
      * Whether a user can destroy (permanently delete) a password.
      */
-    public function destroy(User $user, Password $password): bool
+    public function destroy(User $user, Password $password): Response
     {
         if ($user->hasPermission('password.destroy.any')) {
-            return true;
+            return $this->allow("You can destroy any password.");
         }
 
         if ($user->hasPermission('password.destroy.self') &&
             $password->created_by === $user->id) {
-            return true;
+            return $this->allow("You can destroy this password.");
         }
 
-        return false;
+        return $this->deny("You are unauthorized to destroy this password.");
     }
 
     /**
      * Whether a user can list all passwords.
      */
-    public function viewAll(User $user): bool
+    public function viewAll(User $user): Response
     {
-        return $user->hasPermission('password.view.any');
+        if ($user->hasPermission('password.view.any')) {
+            return $this->allow("You can view any password.");
+        }
+
+        return $this->deny("You are unauthorized to view any password.");
     }
 
     /**
      * Whether a user can view a specific password.
      */
-    public function view(User $user, Password $password): bool
+    public function view(User $user, Password $password): Response
     {
         if ($user->can('viewAll', Password::class)) {
-            return true;
+            return $this->allow("You can view any password");
         }
 
         if ($user->hasPermission('password.view.self') &&
             $password->created_by === $user->id) {
-            return true;
+            return $this->allow("You can view this password.");
         }
 
-        return DB::table('shared_access')
+        $shared = DB::table('shared_access')
             ->where([
                 'password_id' => $password->id,
                 'model_type' => User::class,
                 'model_id' => $user->id,
             ])
             ->exists();
+
+        if ($shared) {
+            return $this->allow("This password was shared with you.");
+        }
+
+        return $this->deny("You are unauthorized to view this password.");
     }
 
     /**
      * Whether a user can share a password.
      */
-    public function share(User $user, Password $password): bool
+    public function share(User $user, Password $password): Response
     {
         if ($user->hasPermission('password.share.any')) {
-            return true;
+            return $this->allow("You can share any password.");
         }
 
         if ($user->hasPermission('password.share.self') &&
             $password->created_by === $user->id) {
-            return true;
+            return $this->allow("You can share this password.");
         }
 
-        return false;
+        return $this->deny("You are unauthorized to share this password.");
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -6,6 +6,7 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 /**
  * Class UserPolicy.
@@ -17,67 +18,80 @@ class UserPolicy
     /**
      * Whether a user can create another user.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        return $user->hasPermission('user.create');
+        if ($user->hasPermission('user.create')) {
+            return $this->allow("You can create users.");
+        }
+
+        return $this->deny("You are unauthorized to create users.");
     }
 
     /**
      * Whether a user can view another user.
      */
-    public function view(User $user, User $subject): bool
+    public function view(User $user, User $subject): Response
     {
-        return $user->hasPermission('user.view.any') ||
-            $user->id === $subject->id;
+        if ($user->hasPermission('user.view.any') || $user->id === $subject->id) {
+            return $this->allow("You can view this user.");
+        }
+
+        return $this->deny("You are unauthorized to view this user.");
     }
 
     /**
      * Whether a user can delete another user.
      */
-    public function delete(User $user, User $subject): bool
+    public function delete(User $user, User $subject): Response
     {
         if ($user->hasPermission('user.delete.any')) {
-            return true;
+            return $this->allow("You can delete any user.");
         }
 
-        if ($user->hasPermission('user.delete.self') &&
-            $user->id === $subject->id) {
-            return true;
+        if ($user->hasPermission('user.delete.self') && $user->id === $subject->id) {
+            return $this->allow("You are allowed to delete your own account.");
         }
 
-        return false;
+        return $this->deny("You are unauthorized to delete this user.");
     }
 
     /**
      * Whether a user can update another user.
      */
-    public function update(User $user, User $subject): bool
+    public function update(User $user, User $subject): Response
     {
         if ($user->hasPermission('user.edit.any')) {
-            return true;
+            return $this->allow("You can update any user.");
         }
 
-        if ($user->hasPermission('user.edit.self') &&
-            $user->id === $subject->id) {
-            return true;
+        if ($user->hasPermission('user.edit.self') && $user->id === $subject->id) {
+            return $this->allow("You are allowed to update your own account.");
         }
 
-        return false;
+        return $this->deny("You are unauthorized to update this user.");
     }
 
     /**
      * Whether a user can promote another user to admin.
      */
-    public function promote(User $user, User $subject): bool
+    public function promote(User $user, User $subject): Response
     {
-        return $user->hasPermission('user.promote') && !$subject->hasRole('admin');
+        if ($user->hasPermission('user.promote') && !$subject->hasRole('admin')) {
+            return $this->allow("You can promote this user to admin.");
+        }
+
+        return $this->deny("You are unauthorized to promote this user to admin.");
     }
 
     /**
      * Whether a user can demote an admin to a user.
      */
-    public function demote(User $user, User $subject): bool
+    public function demote(User $user, User $subject): Response
     {
-        return $user->hasPermission('admin.demote') && $subject->hasRole('admin');
+        if ($user->hasPermission('admin.demote') && $subject->hasRole('admin')) {
+            return $this->allow("You can demote this user.");
+        }
+
+        return $this->deny("You are unauthorized to demote this user.");
     }
 }
