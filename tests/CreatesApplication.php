f361ef4eaefcd11424a2f1298835dbbeee0c2ae6
Ricardo Boss <contact@ricardoboss.de>
2020-01-04 08:35:49 +0100

    Added assertApiResponse macro to TestResponse

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -5,6 +5,7 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\TestResponse;
 
 /**
  * Trait CreatesApplication.
@@ -22,6 +23,21 @@ trait CreatesApplication
 
         $app->make(Kernel::class)->bootstrap();
 
+        $this->registerMacros();
+
         return $app;
+    }
+
+    private final function registerMacros() {
+        TestResponse::macro('assertApiResponse', function (bool $success = true) {
+            /** @var TestResponse $this */
+            $this->assertJsonStructure([
+                'success',
+                'data',
+            ]);
+
+            /** @var TestResponse $this */
+            return $this->assertJsonPath('success', $success, true);
+        });
     }
 }
