9c049a7e0887fff876ba360d1872ad4c939c2845
Ricardo Boss <contact@ricardoboss.de>
2020-01-04 14:20:15 +0100

    Added tabindexes and required to LoginForm

--- resources/js/components/LoginForm.vue
+++ resources/js/components/LoginForm.vue
@@ -2,20 +2,31 @@
     <form @submit.prevent="doLogin">
         <div class="form-group">
             <label for="login-email">E-Mail Address</label>
-            <input class="form-control" id="login-email" type="email" autocomplete="username" v-model="form_email" />
+            <input class="form-control"
+                   id="login-email"
+                   type="email"
+                   autofocus
+                   autocomplete="username"
+                   v-model="form_email"
+                   required
+                   tabindex="1"
+            />
         </div>
         <div class="form-group">
             <label for="login-password">Password</label>
-            <input
-                class="form-control"
-                id="login-password"
-                type="password"
-                autocomplete="current-password"
-                v-model="form_password"
+            <input class="form-control"
+                   id="login-password"
+                   type="password"
+                   autocomplete="current-password"
+                   v-model="form_password"
+                   required
+                   tabindex="2"
             />
         </div>
 
-        <button class="btn btn-primary float-right" type="submit">Login</button>
+        <button class="btn btn-primary float-right" type="submit" tabindex="3">
+            Login
+        </button>
     </form>
 </template>
 
